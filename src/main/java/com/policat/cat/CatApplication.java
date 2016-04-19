d8782f70e7ccfebb971744970b999469ec778e40
MelaniaNitu <melania.nitu@yahoo.com>
2016-04-19 19:06:54 +0300

    Design + Facebook base

--- src/main/java/com/policat/cat/CatApplication.java
+++ src/main/java/com/policat/cat/CatApplication.java
@@ -1,10 +1,12 @@
 package com.policat.cat;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.social.SocialWebAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={SocialWebAutoConfiguration.class})
 public class CatApplication {
 
 	public static void main(String[] args) {
