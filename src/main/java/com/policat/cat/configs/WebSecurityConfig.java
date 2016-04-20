511834b7af36d3475e09490cc36a60d894082bf2
Gabriel Alecu <gc.alecu@gmail.com>
2016-04-20 12:06:05 +0300

    Fixed password storage

--- src/main/java/com/policat/cat/configs/WebSecurityConfig.java
+++ src/main/java/com/policat/cat/configs/WebSecurityConfig.java
@@ -1,19 +1,34 @@
 package com.policat.cat.configs;
 
+import com.policat.cat.auth.AuthedUser;
+import com.policat.cat.entities.User;
+import com.policat.cat.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
-    private UserDetailsService userDetailsService;
+    private UserService userService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -28,6 +43,24 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
+        auth.userDetailsService(userService);
+        auth.authenticationProvider(new AuthenticationProvider() {
+            @Override
+            public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+                String username = (String) authentication.getPrincipal();
+                String providedPassword = (String) authentication.getCredentials();
+                AuthedUser authedUser = userService.findAndAuthenticateUser(username, providedPassword);
+                if (authedUser == null) {
+                    throw new BadCredentialsException("Username/Password does not match for " + authentication.getPrincipal());
+                }
+
+                return new UsernamePasswordAuthenticationToken(authedUser, null, authedUser.getAuthorities());
+            }
+
+            @Override
+            public boolean supports(Class<?> authentication) {
+                return true;
+            }
+        });
     }
 }
--- src/main/java/com/policat/cat/services/UserService.java
+++ src/main/java/com/policat/cat/services/UserService.java
@@ -1,21 +1,54 @@
 package com.policat.cat.services;
 
+import com.policat.cat.auth.AuthedUser;
 import com.policat.cat.temp_containers.UserRegistrationDTO;
 import com.policat.cat.entities.User;
 import com.policat.cat.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
     @Autowired
     private UserRepository repository;
 
-    public User registerUserAccount(UserRegistrationDTO userRegistrationDTO)  {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Override
+    public AuthedUser loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        return new AuthedUser(user);
+    }
+
+    public User create(User user, String password) {
+        user.setPassword(passwordEncoder.encode(password));
+        return userRepository.save(user);
+    }
+
+    public AuthedUser findAndAuthenticateUser(String username, String providedPassword) {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            return null;
+        }
+
+        if (passwordEncoder.matches(providedPassword, user.getPassword())) {
+            return new AuthedUser(user);
+        }
+
+        return null;
+    }
+
+    public User registerUserAccount(UserRegistrationDTO userRegistrationDTO) {
         User user = new User();
         user.setUsername(userRegistrationDTO.getUsername());
-        user.setPassword(userRegistrationDTO.getPassword());
-        return repository.save(user);
+        return create(user, userRegistrationDTO.getPassword());
     }
 }
