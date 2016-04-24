c6bce641c205b727fd4c443f009b8577eafb9a15
Gabriel Alecu <gc.alecu@gmail.com>
2016-04-25 00:02:23 +0300

    Added database setup scripts

---
+++ db/structure.sql
@@ -0,0 +1,49 @@
+
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `password` varchar(255) NOT NULL,
+  `role` varchar(255) DEFAULT NULL,
+  `username` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_sb8bbouer5wak8vyiiy4pf2bx` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `domain` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_imux4m190glnqxeh8gapkk2t` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `question` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `score` int(11) NOT NULL,
+  `text` varchar(255) NOT NULL,
+  `domain_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_d0yhf4xgi1a0y6m5v1vqexwq4` (`domain_id`),
+  CONSTRAINT `FK_d0yhf4xgi1a0y6m5v1vqexwq4` FOREIGN KEY (`domain_id`) REFERENCES `domain` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=206 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `option` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `correct` bit(1) NOT NULL,
+  `text` varchar(255) NOT NULL,
+  `question_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_eix9du6u2r4wxwu415wq8yb99` (`question_id`),
+  CONSTRAINT `FK_eix9du6u2r4wxwu415wq8yb99` FOREIGN KEY (`question_id`) REFERENCES `question` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=806 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `quiz_result` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `date` datetime NOT NULL,
+  `score` int(11) NOT NULL,
+  `domain_id` bigint(20) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_b040f9cqq1oxnjahhi59jyy8v` (`domain_id`),
+  KEY `FK_59ramm9rk8gpqv1oim47sl0v8` (`user_id`),
+  CONSTRAINT `FK_59ramm9rk8gpqv1oim47sl0v8` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_b040f9cqq1oxnjahhi59jyy8v` FOREIGN KEY (`domain_id`) REFERENCES `domain` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=82 DEFAULT CHARSET=latin1;
