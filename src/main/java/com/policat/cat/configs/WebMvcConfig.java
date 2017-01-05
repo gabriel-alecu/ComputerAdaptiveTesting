a02e6df46a1731cee8fe6b35b084d14eb760b79f
MelaniaNitu <melania.nitu@yahoo.com>
2017-01-05 22:35:59 +0200

    added results, co-author and modified contact

--- src/main/java/com/policat/cat/configs/WebMvcConfig.java
+++ src/main/java/com/policat/cat/configs/WebMvcConfig.java
@@ -13,6 +13,7 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
         registry.addViewController("/index").setViewName("index");
         registry.addViewController("/login").setViewName("login");
         registry.addViewController("/about").setViewName("about-page");
+        registry.addViewController("/results").setViewName("results");
         registry.addViewController("/contact").setViewName("contact");
         registry.addViewController("/tutorials").setViewName("tutorials");
     }
