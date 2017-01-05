255d0c9bf2dfa9443d40d4b9fb13fa3e21dc1bad
MelaniaNitu <melania.nitu@yahoo.com>
2017-01-05 22:54:22 +0200

    fixed error

--- src/main/java/com/policat/cat/configs/WebSecurityConfig.java
+++ src/main/java/com/policat/cat/configs/WebSecurityConfig.java
@@ -33,7 +33,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/index", "/register/**", "/about","/results", "/contact", "/tutorials", "/css/**", "/images/**").permitAll()
+                .antMatchers("/", "/index", "/register/**", "/about", "/results", "/contact", "/tutorials", "/css/**", "/images/**").permitAll()
                 .antMatchers("/managedb/**").hasAuthority("ADMIN")
                 .anyRequest().authenticated()
                 .and().formLogin().loginPage("/login").permitAll()
